c77dc881218e0605e0b06a3b8b7b1c1a2defae11
Hayden Fowler <8>
2019-11-28 10:37:52 +0000

    Database Working

--- TH2_CoreMVC_Contoso/Data/Migrations/20191128103602_FirstMigration.Designer.cs
+++ TH2_CoreMVC_Contoso/Data/Migrations/20191128103602_FirstMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TH2_CoreMVC_Contoso.Data;
 
 namespace TH2_CoreMVC_Contoso.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191128103602_FirstMigration")]
+    partial class FirstMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
